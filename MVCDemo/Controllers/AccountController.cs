208f4d5c68f2512c64dd314661e7fb20f4d36fb8
hasee <hasee@PINKOR-PC>
2017-02-13 00:40:59 +0800

    完成教程二

--- MVCDemo/Controllers/AccountController.cs
+++ MVCDemo/Controllers/AccountController.cs
@@ -16,11 +16,18 @@ namespace MVCDemo.Controllers
 
         public ActionResult Login()
         {
+            ViewBag.LoginState = "登录前。。。";
             return View();
         }
         [HttpPost]
         public ActionResult Login(FormCollection fc)
         {
+            // 获取表单数据
+            var email = fc["inputEmail3"];
+            var password = fc["inputPassword3"];
+
+            // 进行下一步处理，这里先改一下名字
+            ViewBag.LoginState = email + "登陆后。。。";
             return View();
         }
 
