4762f0139f34bc2c2ba2a028888841d771982bbc
hasee <hasee@PINKOR-PC>
2017-02-13 00:14:21 +0800

    添加项目文件。

---
+++ MVCDemo/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
